fdadef709876fbcfe113ac5e3c21104259ac94f2
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-06 16:30:13 +0530

    video complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,35 @@
-import { useState } from 'react'
+import { useState} from 'react'
 import '../css/Hero.css'
+import video1 from '../assets/v1.mp4';
+import video2 from '../assets/v2.mp4';
+import video3 from '../assets/v3.mp4';
 
 const Hero = () => {
     // Heading for carousel
     const heroHeadingArr = [{
-        imgSource: "../assets/v1.mp4",
+        imgSource: video1,
         h1: "All Your Information in One Place",
         h2: "Simplify Management with a Unified Data Platform."
     },
     {
-        imgSource: "../assets/v2.mp4",
+        imgSource: video2,
         h1: "Lower Costs, Higher Value",
         h2: "Offering Premium Features at a Fraction of the Price."
     },
     {
-        imgSource: "../assets/v3.mp4",
+        imgSource: video3,
         h1: "Access Anytime, Anywhere",
         h2: "Empowering You with Data on Demand Across Devices."
     }
     ]
     const [heroheading, setHeroHeading] = useState({})
 
+    
     return (
         <section className="hero">
-           <video autoPlay loop>
-            <source src='../assets/v1.mp4' type='video/mp4'/>
-           </video>
+            <video autoPlay loop muted >
+                <source src={video1} type='video/mp4' />
+            </video>
         </section>
     )
 }
